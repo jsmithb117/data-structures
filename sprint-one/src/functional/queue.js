d43a8d51847566805528681f0508abab371c34a9
jsmithb117 <jsmithb117@gmail.com>
2020-10-21 20:24:33 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,40 @@
 var Queue = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
-
+  //create storage.length, set to 0;
+  storage.length = 0;
   someInstance.enqueue = function(value) {
+    //create 'currentIndex' variable, instantiate to storage.length
+    var currentIndex = storage.length;
+    //while 'currentIndex' is greater than zero
+    while (currentIndex > 0) {
+      //set storage[currentIndx + 1] as storage[currentIndex]
+      storage[currentIndex + 1] = storage[currentIndex];
+      //decrement currentIndex
+      currentIndex--;
+    }
+    //set value as storage[1]
+    storage[1] = value;
+    //increment storage.length
+    storage.length++;
   };
 
   someInstance.dequeue = function() {
+    //if storage.length is greater than zero
+    if (storage.length > 0) {
+      //create output variable, set as last element of storage
+      var output = storage[storage.length];
+      //delete last element of storage
+      delete storage[storage.length];
+      //decrement storage.length
+      storage.length--;
+      //return output
+      return output;
+    }
   };
 
   someInstance.size = function() {
+    return storage.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,33 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
+  //create 'length' property of storage, set to 0
+  storage.length = 0;
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    //increment storage.length
+    storage.length++;
+    //add 'value' at storage.length
+    storage[storage.length] = value;
   };
 
   someInstance.pop = function() {
+    //if storage.length is greater than 0
+    if (storage.length > 0) {
+      //create 'output', set to most recently added element
+      var output = storage[storage.length];
+      //remove most recently added element
+      delete storage[storage.length];
+      //decrement storage.length
+      storage.length--;
+      //return output
+      return output;
+    }
   };
 
   someInstance.size = function() {
+    //return storage.length
+    return storage.length;
   };
 
   return someInstance;
