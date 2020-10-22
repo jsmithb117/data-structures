cf0c0ca599816162d64cb2a2df491549f75ecc18
jsmithb117 <jsmithb117@gmail.com>
2020-10-21 20:28:26 -0700

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,38 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {
+    length: 0
+  };
 };
 
+Queue.prototype.enqueue = function(value) {
+  //create 'currentIndex' variable, set as this.storage.length
+  var currentIndex = this.storage.length;
+  //while 'currentIndex' is greater than 0
+  while (currentIndex > 0) {
+    //set this.storage[currentIndex + 1] as this.storage[currentIndex]
+    this.storage[currentIndex + 1] = this.storage[currentIndex];
+    //decrement currentIndex
+    currentIndex--;
+  }
+  //set this.storage[1] as value
+  this.storage[1] = value;
+  //increment this.storage.length
+  this.storage.length++;
+};
 
+Queue.prototype.dequeue = function(value) {
+  //if this.storage.length is greater than 0
+  if (this.storage.length > 0) {
+    //create output variable, set as last element of this.storage
+    var output = this.storage[this.storage.length];
+    //delete last element of this.storage
+    delete this.storage[this.storage.length];
+    //decrement this.storage.length
+    this.storage.length--;
+    //return output
+    return output;
+  }
+};
+Queue.prototype.size = function(value) {
+  return this.storage.length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {
+    length: 0
+  };
 };
 
+Stack.prototype.push = function(value) {
+  //increment this.storage.length
+  this.storage.length++;
+  //add 'value' at this.storage.length
+  this.storage[this.storage.length] = value;
+};
 
+Stack.prototype.pop = function() {
+  //if this.storage.length is greater than 0
+  if (this.storage.length > 0) {
+    //create 'output', set to most recently added element
+    var output = this.storage[this.storage.length];
+    //remove most recently added element
+    delete this.storage[this.storage.length];
+    //decrement this.storage.length
+    this.storage.length--;
+    //return output
+    return output;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.storage.length;
+};
