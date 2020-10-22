f815bb9228275dcf6e7f492660bd8406944e238c
jsmithb117 <jsmithb117@gmail.com>
2020-10-21 20:28:53 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,41 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {
+      length: 0
+    };
   }
 
+  enqueue(value) {
+    //create 'currentIndex' variable, instantiate to this.storage.length
+    var currentIndex = this.storage.length;
+    //while 'currentIndex' is greater than zero
+    while (currentIndex > 0) {
+      //set this.storage[currentIndx + 1] as this.storage[currentIndex]
+      this.storage[currentIndex + 1] = this.storage[currentIndex];
+      //decrement currentIndex
+      currentIndex--;
+    }
+    //set value as this.storage[1]
+    this.storage[1] = value;
+    //increment this.storage.length
+    this.storage.length++;
+  }
+
+  dequeue() {
+    //if this.storage.length is greater than zero
+    if (this.storage.length > 0) {
+      //create output variable, set as last element of this.storage
+      var output = this.storage[this.storage.length];
+      //delete last element of this.storage
+      delete this.storage[this.storage.length];
+      //decrement this.storage.length
+      this.storage.length--;
+      //return output
+      return output;
+    }
+  }
+
+  size() {
+    return this.storage.length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,32 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {
+      length: 0
+    };
   }
 
+  push(value) {
+    //increment this.storage.length
+    this.storage.length++;
+    //add 'value' at this.storage.length
+    this.storage[this.storage.length] = value;
+  }
+
+  pop() {
+    //if this.storage.length is greater than 0
+    if (this.storage.length > 0) {
+      //create 'output', set to most recently added element
+      var output = this.storage[this.storage.length];
+      //remove most recently added element
+      delete this.storage[this.storage.length];
+      //decrement this.storage.length
+      this.storage.length--;
+      //return output
+      return output;
+    }
+  }
+
+  size() {
+    return this.storage.length;
+  }
 }
