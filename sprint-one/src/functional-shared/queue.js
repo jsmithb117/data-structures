443866184643e8483359c187da000496507b1ac7
jsmithb117 <jsmithb117@gmail.com>
2020-10-21 20:27:02 -0700

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {
+      length: 0
+    }
+  };
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    //create currentIndex, set as storage.length
+    var currentIndex = this.storage.length;
+    ////move all elements up one to make room for the new value
+    //while currentIndex is greater than 0
+    while (currentIndex > 0) {
+      //set storage[currentIndex + 1] as storage[currentIndex]
+      this.storage[currentIndex + 1] = this.storage[currentIndex];
+      //decrement currentIndex
+      currentIndex--;
+    }
+    //set value as first element in storage
+    this.storage[1] = value;
+    //increment this.storage.length
+    this.storage.length++;
+  },
+  dequeue: function() {
+    //if storage.length is greater than zero
+    if (this.storage.length > 0) {
+      // console.log ('storage: ', this.storage)
+      //create output variable, set as last element
+      var output = this.storage[this.storage.length];
+      // console.log ('output1: ', output)
+      //delete last element
+      delete this.storage[this.storage.length];
+      //decrement storage.length
+      this.storage.length--;
+      //return output
+      // console.log ('output2: ', output)
+      return output;
+    }
+  },
+  size: function() {
+    return this.storage.length;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage : {
+      length: 0
+    }
+  };
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    //increment storage.length;
+    this.storage.length++;
+    //set value to storage at storage.length
+    this.storage[this.storage.length] = value;
+  },
+  pop: function() {
+    //if storage.length is greater than zero
+    if (this.storage.length > 0) {
+      //create 'output', set as most recent element(first)
+      var output = this.storage[this.storage.length];
+      //delete most recent element
+      delete this.storage[this.storage.length];
+      //decrement this.storage.length
+      this.storage.length--;
+      //return output
+      return output;
+    }
+  },
+  size: function() {
+    return this.storage.length;
+  }
+};
 
 
