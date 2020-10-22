d4c8ebf0f619ac986ad9e6ae8845c1347e9590f4
jsmithb117 <jsmithb117@gmail.com>
2020-10-21 20:27:45 -0700

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  //create new instance with length property set initially to 0
+  someInstance.storage = {
+    length: 0
+  };
+  //return new instance
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    //create index variable, set as current length
+    var currentIndex = this.storage.length;
+    //iterate queue
+    while (currentIndex > 0) {
+      //set current element as next higher indexed element
+      this.storage[currentIndex + 1] = this.storage[currentIndex];
+      //decrement current index
+      currentIndex--;
+    }
+    //set value as first element of storage
+    this.storage[1] = value;
+    //increment length property
+    this.storage.length++;
+  },
+  dequeue: function() {
+    //check if queue is empty
+    if (this.storage.length > 0) {
+      //create output, set as last element in storage
+      var output = this.storage[this.storage.length];
+      //decrement length property
+      this.storage.length--;
+      //return output
+      return output;
+    }
+  },
+  size: function() {
+    return this.storage.length;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  //create new instance with length property set initially to 0
+  someInstance.storage = {
+    length: 0
+  };
+  //return new instance
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    //increment length
+    this.storage.length++;
+    //set value at new length index
+    this.storage[this.storage.length] = value;
+  },
+  pop: function() {
+    //check for length > 0 so you can't pop the zeroth element
+    if(this.storage.length > 0) {
+      //set output as the last element that was pushed to the stack
+      var output = this.storage[this.storage.length];
+      //delete the last element that was pushed to the stack
+      delete this.storage[this.storage.length];
+      //decrement length
+      this.storage.length--;
+      //return output
+      return output;
+    }
+  },
+  size: function() {
+    return this.storage.length;
+  }
+};
 
 
